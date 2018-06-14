4fc10083d375c2f01280c883b0062bb911f124b0
Merias <hugo.merias@lapiscine.pro>
2018-06-14 16:47:10 +0200

    Mise en place des fichiers de base

---
+++ form.php
@@ -0,0 +1,5 @@
+		<form method="post" action="index.php">
+			Nom: <input type="text" name="nom"><br>
+			Prénom: <input type="text" name="nom"><br>
+			Mail: <input type="text" name="nom"><br>
+		<input type="submit" value="Submit">
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+	// Création de la fonction afficherTexte
+	function afficherMoyenne($val1,$val2,$val3){
+		//Contenu de la fonction
+		$calcul = ($val1 + $val2 + $val3)/3;
+		return $calcul;
+	}
+?>
+
+<!doctype html>
+<html>
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+	<form method="post" action="index.php">
+		<input type="number" name="nbr1" min="1" max="100">
+		<input type="number" name="nbr2" min="1" max="100">
+		<input type="number" name="nbr3" min="1" max="100">
+		<input type="submit" value="Submit">
+	</form>
+	<h1>
+		<?php
+		// Appel de la fonction
+			echo afficherMoyenne($_POST['nbr1'],$_POST['nbr2'],$_POST['nbr3']);
+		?>
+	</h1>
+
+</body>
+</html>
